f0894d79d46cbf8722d2703e9e54940493d2ab92
Your Name <your.email@example.com>
2026-01-31 21:57:27 +0530

    fix: revert homepage and static serving for Vercel

--- server/server.js
+++ server/server.js
@@ -62,20 +62,6 @@ app.get('/api', (req, res) => {
     res.send('Team Vortex API is Running');
 });
 
-// Serve Static Assets in Production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static(path.join(__dirname, '../build')));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
-    });
-} else {
-    app.get('/', (req, res) => {
-        res.send('Team Vortex API is Running (Development Mode)');
-    });
-}
-
 // Start Server
 if (process.env.NODE_ENV !== 'production') {
     app.listen(PORT, () => {
